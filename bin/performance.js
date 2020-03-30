85c2df496f0dfaee67ee4dad3ca83cf158182ac3
Alex Fernández <alejandrofer@gmail.com>
2020-03-30 14:10:18 +0200

    Add default options.

--- bin/performance.js
+++ bin/performance.js
@@ -13,9 +13,9 @@ var packageJson = require(__dirname + '/../package.json');
 
 // init
 var options = stdio.getopt({
-	file: {key: 'f', args: 1, description: 'Output results to a file'},
+	file: {key: 'f', args: 1, description: 'Output results to a file', default: null},
 	json: {key: 'j', description: 'Show benchmark results in JSON format'},
-	seconds: {key: 's', args: 1, description: 'Number of seconds to run'},
+	seconds: {key: 's', args: 1, description: 'Number of seconds to run', default: 0},
 	version: {key: 'v', description: 'Show version number and exit'},
 });
 if (options.version)
